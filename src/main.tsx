4e8f669953754539cb54674ab7084117f76aee1a
thomasvt <thomas.van.tilburg@nl.abnamro.com>
2024-04-16 18:50:53 +0200

    Cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import ReactDOM, { Root as ReactRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-let root: ReactRoot | undefined;
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-unused-vars, @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-member-access
-(window as any).renderAdvertising = (
-  containerId: string,
-  _history: any,
-  _keepAlive: any,
-  _user: any
-) => {
-  root = ReactDOM.createRoot(document.getElementById(containerId)!);
-  root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  );
-};
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-unused-vars, @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-member-access
-(window as any).unmountAdvertising = (_containerId: string) => {
-  document.body.className = 'hideScroll';
-  root?.unmount();
-};
+    </React.StrictMode>,
+)
