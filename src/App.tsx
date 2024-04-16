9ba7d4e3453e00660b36a5754b586ef1dc7f622a
thomasvt <thomas.van.tilburg@nl.abnamro.com>
2024-04-16 18:45:20 +0200

    Make it break!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-// import './App.css';
 import * as stylex from '@stylexjs/stylex';
 
 const styles = stylex.create({
-  abc: {},
+  abc: {
+      borderWidth: '10px'
+  },
 });
 
 function App() {
